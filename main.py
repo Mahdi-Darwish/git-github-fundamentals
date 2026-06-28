c772b02bbb2b46cc94287071f78acea757706e6e
Mahdi Darwish <mahdidarwish@Mahdis-MacBook-Pro.local>
2026-06-29 00:43:15 +0300

    Added a new evensquare function

--- main.py
+++ main.py
@@ -4,3 +4,21 @@ def add(x,y):
 
 def subtract(x,y):
     return x -y,
+
+#list example
+numbers=[1,2,3,4]
+squares=[x**2 for x in numbers if x%2 ==0]
+print(squares)
+
+#dictionary example
+d={'cat':4,'bird':2,'snake':0}
+for animals,legs in d.items():
+    print(f"The {animals} has {legs} legs")
+
+    def evensquares():
+        x=(int(input('Enter a number:')))
+        if x **2 %2 ==0:
+            print('The answer is even') 
+        else: print('The answer is odd')
+evensquares(),
+
